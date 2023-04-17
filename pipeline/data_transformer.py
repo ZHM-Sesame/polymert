6e1c84b00d8f0b860e92843939d96b0dc28aa539
siddharthgoe_umass_edu <siddharthgoe_umass_edu@gypsum-gpu073.unity.rc.umass.edu>
2023-04-17 07:47:43 +0000

    Run model for random copolymers

--- pipeline/data_transformer.py
+++ pipeline/data_transformer.py
@@ -1,28 +1,37 @@
 import numpy as np
 import pandas as pd
 
-def concatenate_embeddings(row, embeddings):
+
+def concatenate_embeddings(row, embeddings, repetitions):
+    """
+    Arranges two input lists a and b into a list of lists of length l,
+    according to the given ratio and arrangement type.
+    Ratio is a float value between 0 and 1 representing the proportion of a to b
+    in the final list of lists.
+    Arrangement type can be 'random', 'block', or 'alternating'.
+    """
     mul_a = 1 if row['fracA'] < row['fracB'] else int(row['fracA'] / row['fracB'])
     mul_b = 1 if row['fracB'] < row['fracA'] else int(row['fracB'] / row['fracA'])
 
+    arrangement_type = row['chain_arch']
+    
     emb_a, emb_b = np.array(embeddings[row['monoA']]), np.array(embeddings[row['monoB']])
-    rep_a, rep_b = np.tile(emb_a, mul_a), np.tile(emb_b, mul_b)
+        
+    rep_a = np.tile(emb_a, (mul_a, 1))
+    rep_b = np.tile(emb_b, (mul_b, 1))
+    rep_unit = np.concatenate((rep_a, rep_b), axis = 0)
+    repeated_polymer = np.tile(rep_unit , (int(repetitions / (mul_a + mul_b)), 1))
+
+    if repetitions % (mul_a + mul_b) != 0:
+        repeated_polymer = np.concatenate((repeated_polymer, np.tile(emb_a, (repetitions % (mul_a + mul_b), 1))), axis = 0)
     
-    if mul_a == mul_b:
-        if row['chain_arch'] == 'block':
-            rep_a = np.tile(emb_a, 2)
-            rep_b = np.tile(emb_b, 2)
-            op = np.concatenate((rep_a, rep_b), axis=None)
-        else:
-            op = np.tile(np.concatenate((rep_a, rep_b), axis=None), 2)   #alternating
-    else:
-        op = np.concatenate((rep_a, rep_b), axis=None)   #block
+    if arrangement_type == 'random':
+        np.random.shuffle(repeated_polymer)
     
-    return op
+    return repeated_polymer
 
 
-
-def get_repeated_polymer(df, embeddings, nbits, repetition_format=None, homopolymer=True, repetitions=100):
+def get_repeated_polymer(df, embeddings, nbits, repetition_format=None, homopolymer=True, repetitions=400):
     if homopolymer:
         fp = df['mono'].apply(lambda smile: embeddings[smile])
     else:
@@ -32,14 +41,11 @@ def get_repeated_polymer(df, embeddings, nbits, repetition_format=None, homopoly
             nbits += unique_archs
             fp = df.apply(lambda x: np.append((np.array(embeddings[x['monoA']]) * x['fracA'] + np.array(embeddings[x['monoB']]) * x['fracB']), [x['chain_arch_alternating'], x['chain_arch_block'], x['chain_arch_random']]), axis=1)
         elif repetition_format == "concat":
-            df = df[df['chain_arch'] != 'random']
-            nbits = nbits * 4
-            fp = df.apply(lambda x: concatenate_embeddings(x, embeddings), axis=1)
-
+            fp = df.apply(lambda x: concatenate_embeddings(x, embeddings, repetitions), axis=1)
+    
     fp = list(fp)
     fp_array = np.asarray([np.array(fp[i]) for i in range(len(fp))])
-    
-    repeated_polymer = np.repeat(fp_array, repetitions, axis=0)
-    repeated_polymer = repeated_polymer.reshape(len(df), repetitions, nbits)
+
+    repeated_polymer = fp_array.reshape(len(df), repetitions, nbits)
     
     return nbits, repeated_polymer, df['value']
